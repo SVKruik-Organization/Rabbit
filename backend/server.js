f023d68fa5fbc070d46a083b3217fd5b782c7e0a
Stefan Kruik <s.kruik@icloud.com>
2024-01-20 17:26:16 +0100

    Server Baseplate Upgraded

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+app.use(express.json());
+const prefix = "/api/v1";
+
+// Import Routes
+const userRoutes = require('./routes/userRoutes');
+app.use(`${prefix}/users`, userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
-app.get("/api", (req, res) => {
+app.get(prefix, (req, res) => {
     res.json({ message: "Hello from server!" });
 });
