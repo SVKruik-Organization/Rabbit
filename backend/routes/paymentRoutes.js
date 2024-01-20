f215f1addc188768d573a6df8507d8624a656b89
donovan-nt <107775412+donovan-nt@users.noreply.github.com>
2024-01-20 23:46:39 +0100

    Stripe Payment API Working

---
+++ backend/routes/paymentRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
+
+const storeItems = new Map([
+    [1, { priceInCents: 1, name: 'Rabbit Plus Subscription' }],
+    [2, { priceInCents: 2, name: 'Rabbit Premium Subscription' }]
+]);
+
+
+router.post('/pay', async function (req, res) {
+    console.log(req.body.items);
+    try {
+        const session = await stripe.checkout.sessions.create({
+            // payment_method_types: ['card', 'klarna', 'ideal', 'paypal'],
+            // payment_method_types: ['card', 'ideal', 'paypal'],
+            payment_method_types: ['card', 'ideal'],
+            mode: 'subscription',
+            // mode: 'payment' // One time payment
+            line_items: req.body.items.map(item => {
+                const storeItem = storeItems.get(item.id);
+                return {
+                    price_data: {
+                        currency: 'eur',
+                        product_data: {
+                            name: storeItem.name
+                        },
+                        unit_amount: storeItem.priceInCents,
+                        recurring: {
+                            interval: 'month',
+                        },
+                    },
+                    quantity: item.quantity
+                }
+            }),
+            success_url: `${process.env.SERVER_URL}/succes.html`,
+            cancel_url: `${process.env.SERVER_URL}/cancel.html`,
+        })
+
+        res.json({ url: session.url })
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+
+});
+
+router.get('/', function (req, res) {
+    res.send('Stripe Homepage');
+});
+
+router.get('/payment', function (req, res) {
+    res.send(process.env.STRIPE_PRIVATE_KEY.toString());
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
+app.use(express.static("public"))
+
 const prefix = "/api/v1";
 
 // Import Routes
 const userRoutes = require('./routes/userRoutes');
 app.use(`${prefix}/users`, userRoutes);
+
+const paymentRoutes = require('./routes/paymentRoutes');
+app.use(`${prefix}/stripe`, paymentRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
